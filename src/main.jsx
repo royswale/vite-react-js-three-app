e1a256e421f9c6e5842fd9992830679adcdd5a47
royswale <royswale@gmail.com>
2022-07-01 17:29:16 +0800

    examples

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-// import App from './App'
+import App from './App'
 import ExploadingCrystal from './pages/ExplodingCrystal'
+import FloatingShoes from './pages/FloatingShoes'
+import Ballpit from './pages/Ballpit'
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <ExploadingCrystal />
+    {/* <ExploadingCrystal /> */}
+    {/* <FloatingShoes /> */}
+    <Ballpit />
   </React.StrictMode>,
   document.getElementById('root')
 )
